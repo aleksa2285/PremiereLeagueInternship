b5dfa26176d5a2261629a85a30ace7379c85db47
aleksa2285 <Stojke36k@gmail.com>
2018-06-06 19:10:49 +0200

    v 0.2
    bugs fixed ( authentication )

--- Spring/edc/src/main/java/com/league/config/SecurityConfig.java
+++ Spring/edc/src/main/java/com/league/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,7 +16,6 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-//@ComponentScan("com.league.config.security")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -23,26 +23,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
 
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailService);
-        authenticationProvider.setPasswordEncoder(encoder());
-        return authenticationProvider;
-    }
-    @Bean
-    public PasswordEncoder encoder(){
-        return new BCryptPasswordEncoder(11);
-    }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.authenticationProvider(authenticationProvider());
-    }
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider(){
+//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//        authenticationProvider.setUserDetailsService(userDetailService);
+//        authenticationProvider.setPasswordEncoder(encoder());
+//        return authenticationProvider;
+//    }
+//    @Bean
+//    public PasswordEncoder encoder(){
+//        return new BCryptPasswordEncoder(11);
+//    }
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-//        auth.jdbcAuthentication().dataSource(dataSource)
-//                .usersByUsernameQuery("select * from USERS where USER_USERNAME=?")
-//                .authoritiesByUsernameQuery("select USER_ROLE from users where USER_USERNAME = ?");
+//        auth.authenticationProvider(authenticationProvider());
+//    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select USER_USERNAME, USER_PASSWORD, true from USERS where USER_USERNAME=?")
+                .authoritiesByUsernameQuery("select 'USER_USERNAME', 'USER_ROLE' from users where USER_USERNAME = ?");
+    }
+//    public void configureGlobal(HttpSecurity httpSecurity) throws Exception{
+//        httpSecurity.authorizeRequests()
+//                .antMatchers("/", "/home").access("hasRole('USER')")
+//                .and().formLogin();
 //    }
 
 }
--- Spring/edc/src/main/java/com/league/data/service/UserService.java
+++ Spring/edc/src/main/java/com/league/data/service/UserService.java
@@ -1,19 +1,13 @@
 package com.league.data.service;
 
-import com.league.data.model.MyUserPrincipal;
+import com.league.data.dao.UserDAO;
 import com.league.data.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Repository
-public class UserService implements UserDetailsService{
+public class UserService implements UserDAO{
 
     private NamedParameterJdbcTemplate namedJdbcOperations;
 
@@ -22,15 +16,23 @@ public class UserService implements UserDetailsService{
         this.namedJdbcOperations = namedParameterJdbcTemplate;
     }
 
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        String sql = "SELECT * from users where USER_USERNAME =:username";
-        Map<String, Object> namedParameters = new HashMap<String, Object>();
-        namedParameters.put("username", username);
-        User user = this.namedJdbcOperations.queryForObject(sql, namedParameters, User.class);
-        if (user == null) throw new UsernameNotFoundException(username);
-        else return new MyUserPrincipal(user);
-
+    public User findByUsername() {
+        return null;
     }
+
+    public String getAuthenticationToken(String username) {
+        return null;
+    }
+
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        String sql = "SELECT * from users where USER_USERNAME =:username";
+//        Map<String, Object> namedParameters = new HashMap<String, Object>();
+//        namedParameters.put("username", username);
+//        User user = this.namedJdbcOperations.queryForObject(sql, namedParameters, User.class);
+//        if (user == null) throw new UsernameNotFoundException(username);
+//        else return new MyUserPrincipal(user);
+//
+//    }
 }
 
